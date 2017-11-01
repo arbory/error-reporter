8fa88fcbeca5d06648e1743a1c38a7ada8c12e19
Janis Osenieks <janis.osenieks@cube.lv>
2017-11-01 15:42:50 +0200

    get request params

--- src/Reporter.php
+++ src/Reporter.php
@@ -20,7 +20,8 @@ class Reporter
     public function reportException(Exception $exception)
     {
         $this->exception = $exception;
-        $params          = $this->getExceptionParamsArray();
+        $exceptionParams = $this->getExceptionParamsArray();
+        $requestParams   = $this->getRequestParamsArray();
     }
 
     protected function getExceptionParamsArray()
@@ -31,6 +32,22 @@ class Reporter
             'line'       => $this->exception->getLine(),
             'level'      => $this->exception->getCode(),
             'stackTrace' => $this->exception->getTrace(),
+        ];
+    }
+
+    protected function getRequestParamsArray()
+    {
+        return [
+            'user_ip'           => array_get($_SERVER, 'REMOTE_ADDR'),
+            'user_forwarded_ip' => array_get($_SERVER, 'HTTP_X_FORWARDED_FOR'),
+            'http_host'         => array_get($_SERVER, 'HTTP_HOST'),
+            'request_uri'       => array_get($_SERVER, 'REQUEST_URI'),
+            'query_string'      => array_get($_SERVER, 'QUERY_STRING'),
+            'request_method'    => array_get($_SERVER, 'REQUEST_METHOD'),
+            'http_referer'      => array_get($_SERVER, 'HTTP_REFERER'),
+            'user_agent'        => array_get($_SERVER, 'HTTP_REFERER'),
+            'http_content_type' => array_get($_SERVER, 'CONTENT_TYPE'),
+            'http_cookie'       => array_get($_SERVER, 'HTTP_COOKIE')
         ];
     }
 }
